04a8abfac08af28236d73e6390edeb6f933b3f9f
gongahkia <angryapplegravy@gmail.com>
2023-11-13 19:43:53 +0800

    lovelyAndThanks
    (cherry picked from commit d2d616dbbfe0702ed5b2efbbd683257ccbba984e)
    (cherry picked from commit 96341d02058dc0eeb3a20be5c2dfc865a6f9f0bb)

--- src/senko.sh
+++ src/senko.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-python3.11 main.py
+# checks for python3.11 interpreter on device 
+if command -v python3.11 &> /dev/null; then 
+    python3.11 main.py
+else
+    echo "python3.11 not found on device, please install first."
+fi

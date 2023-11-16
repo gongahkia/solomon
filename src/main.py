b30cb994db3f8ee46bfca0035c0c53248df9aadf
gongahkia <angryapplegravy@gmail.com>
2023-11-16 23:05:42 +0800

    added two functions
    (cherry picked from commit 9612b089b2374e28e1295562a90d0980b36e0eea)
    (cherry picked from commit 4c553d3274ad475cca9993d80b626fabfd74afe6)

--- src/main.py
+++ src/main.py
@@ -80,17 +80,58 @@ def read_sko(filename:str) -> {}:
     fhand.close()
     return sko_contents
 
-# FUA reads through the file, creates a curses frontend for users to select which card set they want
-def select_flashcard_set(file_contents:{}) -> {}:
+# reads through the file and allows users to select which card set they want
+def select_flashcard_set(file_contents:{}) -> (str,{}):
+
+    name_array:[str] = [set_name for set_name in file_contents]
+
+    screen = curses.initscr()
+    screen.keypad(True)
+    curses.cbreak()
+    curses.curs_set(0)
+    if curses.has_colors():
+        curses.start_color()
+        curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+    while True:
+        screen.erase()
+        coords:{str:int}= {"x":0, "y":2}
+        counter:int = 1
+        screen.addstr(0, 0, "Type in a valid number", curses.color_pair(3))
+        for flashcard_set in name_array:
+            screen.addstr(coords["y"], coords["x"], f"{counter} | {flashcard_set}")
+            coords["y"] += 1
+            counter += 1
+        keypress = chr(screen.getch())
+        if not keypress.isnumeric() or int(keypress) > len(name_array) or int(keypress) < 1:
+            continue
+        else:
+            screen.erase()
+            screen.addstr(0,0,"Loading your senko set...", curses.color_pair(2))
+            screen.refresh()
+            screen.keypad(False)
+            curses.echo()
+            curses.endwin()
+            return (name_array[int(keypress) - 1], file_contents[name_array[int(keypress) - 1]])
+
+# FUA add code inside to instantiate screen and everything, for reading through a json and presenting qns, takes in input for difficulty of each card
+def render_sko(sko_contents:{}) -> {}:
     pass
 
-# FUA add code inside to instantiate screen and everything, for reading through a json and presenting qns
-def render_sko(sko_contents:{}) -> None:
-    pass
-
-# FUA adds days from card_difficulty from sko_contents to a date, convert to int using format learnt in is111 lab 2, return date in string format for easy checking
+# adds days from card_difficulty from sko_contents to a date
 def add_days(date:str, days_add:int) -> str:
-    pass
+    new_dat:int = int(date.split("/")[0]) + (int(date.split("/")[1]) * 30) + (int(date.split("/")[2]) * 365) + days_add
+    new_year:int = new_dat // 365 
+    new_month:int = (new_dat % 365) // 30
+    new_month_str:str = f"{new_month}"
+    new_day:int = (new_dat % 365) % 30
+    new_day_str:str = f"{new_day}"
+    if new_month < 10:
+        new_month_str:str = f"0{new_month}"
+    if new_day < 10:
+        new_day_str:str = f"0{new_day}"
+    return f"{new_day_str}/{new_month_str}/{new_year}"
 
 # FUA provides the frontend for editing flashcards in curses cli, returns the edited dictionary
 def edit_sko(sko_contents:{}) -> {}:
@@ -112,4 +153,4 @@ def check_sko(filename:str) -> bool:
 def write_sko(filename:str, sko_contents:{}) -> None:
     pass
 
-print(read_sko(select_sko_file()))
+print(select_flashcard_set(read_sko(select_sko_file())))

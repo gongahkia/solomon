bfdec23a2c424977dacb88f5caab83ca58ad43ea
gongahkia <angryapplegravy@gmail.com>
2024-05-09 14:29:43 +0800

    scrapped project structure
    (cherry picked from commit 0ee9cf6571cff6a8b108e408b08969212a42b05c)
    (cherry picked from commit c6a0949b696f37c75e8e7ff8701f7cdde922a6c7)

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 all:build
-	@build
 
 debug:src/main.go
 	@echo "debug mode"
@@ -10,14 +9,11 @@ build:src/main.go
 	@go run src/main.go 
 
 config:
-	@echo "installing monke..."
+	@echo "installing monke's dependancies..."
 	@sudo apt upgrade && sudo apt update && sudo apt autoremove
 	@sudo apt install golang
 	@sudo apt install gcc
-	@ sudo apt install libc6-dev libgl1-mesa-dev libxcursor-dev libxi-dev libxinerama-dev libxrandr-dev libxxf86vm-dev libasound2-dev pkg-config
-	@echo "installation complete, testing ebiten engine..."
-	@GOOS=windows go run github.com/hajimehoshi/ebiten/v2/examples/rotate@latest 
-	@echo "installation validated"
+	@echo "installation complete"
 	@go mod init github.com/gongahkia/monke 
 	@echo "go mod initialized"
 
--- src/main.go
+++ src/main.go
@@ -1,84 +1,25 @@
 // FUA
-    // learn how ebiten api generally works with go
-    // work out project structure
-	// call some api to get a string of random words to type
+    // work out project structure from scratch in the cli without using ebiten
 	// mimick most to all monkeytype features
 	// basic keyboard and punctuation and number inputs, escape escapes the game
 	// highscore with past WPM and player name as needed
-	// if possible, work out how to implement a serverless highscore system
+	// see if i can still get an audio driver in?
 	// add crunchy typing audio like a typewriter perhaps?
+	// have proper code seperation
 
 package main
 
 import (
     "fmt"
-	"os"
-	"log"
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	// "os"
+	// "log"
 )
-
-type Game struct{
-	TextInput string
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) { // pointer receiver function
-	return 320, 240
-}
-
-func (g *Game) KeyPressed(key ebiten.Key, mod ebiten.ModifierKey, text string) error { // pointer receiver function
-
-	// --- user input ---
-
-	if key == ebiten.KeyEscape {
-		fmt.Println("quitting monke...")
-		return fmt.Errorf("user quit monke")
-	} else if key == ebiten.KeyBackspace && len(g.TextInput) > 0 {
-		g.TextInput = g.TextInput[:len(g.TextInput)-1]
-	} else if key == ebiten.KeyEnter {
-		g.TextInput += "\n"
-	} else if len(text) > 0 {
-		g.TextInput += text
-	} else {}
-
-	return nil
-
-}
-
-func (g *Game) Update() error { // pointer receiver function
-
-// --- update logical state ---
-
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) { // pointer receiver function
-
-// --- render graphics ---
-
-	ebitenutil.DebugPrint(screen, "Welcome to MONKE")
-
-}
 
 func main() {
 
+	// --- main code execution ---
 
-// --- window initialization ---
+	fmt.Println("monke")
 
-	ebiten.SetFullscreen(true)
-	ebiten.SetWindowTitle("Monke")
-
-// --- default presets ---
-
-	game := &Game{}
-
-	ebiten.SetKeyHandler(func(key ebiten.Key) {
-		game.KeyPressed(key)
-	})
-
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err) // error logging
-		os.Exit(1)
-	}
 
 }

8b92d7e1a4e7bf94d4670f9e30ecbc573a804db2
gongahkia <angryapplegravy@gmail.com>
2024-05-09 15:16:13 +0800

    further plans
    (cherry picked from commit 84767e12a6e9cfc1a59974bd275bae8d002bf435)
    (cherry picked from commit fa44f809be690d14467c2bc8e3bc423e1372483a)

--- src/main.go
+++ src/main.go
@@ -1,14 +1,12 @@
 // FUA
-    // work out project structure from scratch in the cli without using ebiten
-	// call an api that provides random words to be typed out
 	// mimick most to all monkeytype features
 	// basic keyboard and punctuation and number inputs, escape escapes the game
 	// use a stack for each of the letters, the stack is cleared when a word is correctly entered
 	// use a queue for words that need to be cleared?
 	// highscore with past WPM and player name as needed
 	// see if i can still get an audio driver in?
+	// have proper code seperation and ideally functional programming paradigms would be good
 	// add crunchy typing audio like a typewriter perhaps?
-	// have proper code seperation
 
 package main
 

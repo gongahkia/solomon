d72c2542ca2e500e4c601897f7e90de084ec0e6d
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-12 09:45:45 +0800

    ci: verify source license headers
    (cherry picked from commit 7b255a1ffd9a0aed1944b80f097790761a82c661)

--- packages/solomon-ts/src/index.ts
+++ packages/solomon-ts/src/index.ts
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 export type JsonPrimitive = string | number | boolean | null;
 export type JsonValue = JsonPrimitive | JsonObject | JsonValue[];
 export type JsonObject = { [key: string]: JsonValue };

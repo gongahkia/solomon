6eb0a2d5cf8cd981dfdcb9718288bba0dfcb9b85
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-13 09:15:29 +0800

    build: support current Nix macOS targets
    (cherry picked from commit 7b9ee93d34b933ad766354b5ba9de9fbecc4a907)

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in

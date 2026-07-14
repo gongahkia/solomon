9630e0df270460d40b2fcea0236d06d6f9f6166a
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-14 10:46:09 +0800

    Add SQLite connection factory
    (cherry picked from commit dd564e22e68b759d9a6064f8de8588061c79fee7)

--- src/stonks_cli/vnext/__init__.py
+++ src/stonks_cli/vnext/__init__.py
@@ -1,6 +1,7 @@
 """Isolated vNext decision-support package namespace."""
 
 from stonks_cli.vnext.boundaries import PACKAGE_BOUNDARIES, PackageBoundary, VNextPackage, validate_package_boundaries
+from stonks_cli.vnext.database import SQLiteConnectionFactory
 from stonks_cli.vnext.errors import (
     VNextApplicationError,
     VNextConfigurationError,
@@ -43,6 +44,7 @@ __all__ = [
     "RUNTIME_ROOT_ENV",
     "RunIdentity",
     "SecretReference",
+    "SQLiteConnectionFactory",
     "RuntimeDirectories",
     "RuntimeDirectory",
     "SystemUTCClock",

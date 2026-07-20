5fa5c15daaf0aea21598c4ab60e4dfb73abef4e1
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-20 13:40:20 +0800

    test: serve daemon pre-send decisions (#289)

---
+++ internal/daemon/pre_send_test.go
@@ -0,0 +1,24 @@
+package daemon
+
+import (
+	"context"
+	"testing"
+
+	"github.com/gongahkia/close-enough/internal/config"
+	"github.com/gongahkia/close-enough/internal/packs"
+)
+
+func TestServiceUsesCuratedSafeRewrite(t *testing.T) {
+	resolver, err := packs.NewBundledRuntimeResolver()
+	if err != nil {
+		t.Fatal(err)
+	}
+	service := Service{Config: config.Default(), Packs: resolver}
+	response, err := service.Handle(context.Background(), Request{Version: ProtocolVersion, Operation: PreSendOperation, Command: "git sttaus"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if response.Action != "rewrite" || response.Suggestion != "git status" || response.Source != "curated" || response.RuleID != "git-status-sttaus" {
+		t.Fatalf("response = %+v", response)
+	}
+}
